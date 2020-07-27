f9f8fea6153d25739096a8e84ed2951eab9c8ab5
gonzo8880 <gonzaloscity@gmail.com>
2020-07-27 01:39:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+static void myMethod() {
+  System.out.println("Failure is not an option");
+public class MyClass {
+  public static void main(String[] args) {
+    Level myMar = Level.EASY;
+    System.out.println(myVar);
+  }
+}
+
+public static void main(String[] args) {
+  myMethod();
+public class MyClass {
+  public static void main(String[] args) {
+    Level myVar = Level.HARD;
+    System.out.println(myVar);
+  }
+}
+
+static void myMethod(String fname) {
+  System.out.println(fname + "Orive Villa");
+public class MyClass {
+  public static void main(String[] args) {
+    Level myMar = Level.HARD;
+    System.out.println(myVar);
+  }
+}  
+
+public static void main(String[] args) {
+  myMethod("Gonzalo");
+public class MyClass {
+  public static void main(String[] args) {
+    Level.myVar = Level.MEDIUM;
+    System.out.println(myVar);
+  }
+}
+
+public class MyClass {
+  static void myMethod(String fname, int age) {
+    System.out.println(fname + "is" + age);
+  }
+
+  public static void main(String[] args) {
+    myMethod("Gonzalo," 28);
+    myMethod("Rodrigo," 26);
+    myMethod("Sandy," 80);
+  }
+}
